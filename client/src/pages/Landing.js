c828c1b6b1ddc9220a3b780f1a204b9443b29289
dlew253 <dlew253@yahoo.com>
2020-03-18 14:26:34 -0700

    landing page 2.0

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,31 +1,12 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import PreNavBar from "../Components/PreNavBar";
+import Hero from "../Components/Hero";
+import Footer from "../Components/Footer";
 
 
 const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', 
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+
 }));
 function Landing() {
   return (
