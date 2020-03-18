6982396bdd6ee4c7c560313362627815f3e06cd6
dlew253 <dlew253@yahoo.com>
2020-03-18 16:26:24 -0700

    User sign up initial build

--- client/src/pages/SignupUser.js
+++ client/src/pages/SignupUser.js
@@ -1,9 +1,21 @@
 import React from "react";
+import PreNavBar from "../Components/PreNavBar";
+import UserSignUp from "../Components/UserSignUp";
+import Footer from "../Components/Footer";
 
 function SignupUser() {
   return (
-    <div></div>
+    <div>
+
+      <PreNavBar />
+      <UserSignUp />
+      <Footer />
+
+    </div>
   );
 }
 
 export default SignupUser;
+
+// https://material-ui.com/components/app-bar/
+// https://material-ui.com/getting-started/templates/
