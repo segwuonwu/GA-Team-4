19b0a7d463b0d84a23a3ea7c09915e39db334657
melissay94 <melissay94@yahoo.com>
2020-03-18 20:42:52 -0700

    Added routes for post auth

--- client/src/Components/Content.js
+++ client/src/Components/Content.js
@@ -3,6 +3,9 @@ import { Switch, Route } from "react-router-dom";
 import Landing from "../pages/Landing";
 import Login from "../pages/Login";
 import Signup from "../pages/SignupUser";
+import Home from "../pages/Home";
+import Profile from "../pages/ProfileUser";
+import SearchResults from "../pages/SearchResultsUser";
 
 function Content() {
   return(
@@ -10,6 +13,9 @@ function Content() {
       <Route path="" exact={true} component={Landing} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
+      <Route path="/home" component={Home} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/search" component={SearchResults} />
     </Switch>
   );
 }
