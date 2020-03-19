8dca87a92060b632afb701b079b1497b0cbad7f2
segwuonwu <segwuonwu@gmail.com>
2020-03-19 11:29:03 -0700

    modified the user routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,53 +2,19 @@ const express = require('express');
 const router = express.Router()
 const db = require('../models/index')
 
-
+router.get('/', (req, res)=>{
+    res.send('Welcome to user profile');
+})
 // page to get a list of events
-router.get('/', (req, res) => {
+router.get('/events', (req, res) => {
     db.Event.find({})
         .then(events => {
             res.send(events);
     }).catch(err => res.send({message: 'Error in getting all events', err}))
 })
 
-// router.post('/:id', (req, res) => { 
-//     db.User.create(req.body.id)
-//         .then(event => {
-//         res.send(event)
-//     }).catch(err => {
-//         console.log('Server error', err)
-//         res.status(500).send({ message: 'Server error' })
-//     })
-// })
-
-// router.post('/', (req, res) => {
-//     db.User.findOne(req.user.id)
-//     .then(user => {
-//         user.event.push({
-//             name: req.body.name,
-//             date: req.body.date,
-//             city: req.body.city,
-//             state: req.body.state,
-//             location: req.body.location,
-//             websitel: req.body.website,
-//             detail: req.body.detail
-//         })
-//         // Save the changes to the DB
-//             user.save().then(() => {
-//             res.send({ event: user.event})
-//         })
-//         .catch(err => {
-//             console.log('Aww suck', err)
-//             res.status(503).send({ message: 'Error saving document' })
-//         })
-//     }).catch(err => {
-//         console.log('Server error', err)
-//         res.status(500).send({ message: 'Server error' })
-//       })
-// })
-
 //Show specific Event. 
-router.get('/:id', (req, res) => {
+router.get('/events/:id', (req, res) => {
     db.Event.findById(req.params.id)
         .then(events => {
             res.send(events);
@@ -56,14 +22,14 @@ router.get('/:id', (req, res) => {
 })
 
 // Delete an event
-router.delete('/:id', (req, res) => {
+router.delete('/events/:id', (req, res) => {
     console.log('---Delete route');
     let id = { _id: req.params.id }
     db.Event.deleteOne(id)
     .then(() => {
         console.log('Event deleted')
-        //res.redirect('/users');
-        res.send(`${req.body.eventname} has been removed`)
+        res.redirect('/users');
+        //res.send(`${req.body.eventname} has been removed`)
     })
     .catch(err => res.send({message: 'Error deleting event', err}))
 })
