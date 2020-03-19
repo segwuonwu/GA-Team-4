56524f5172ed42c92598a4f8428c92c7a50d06ae
melissay94 <melissay94@yahoo.com>
2020-03-19 10:03:22 -0700

    Redid search to use iconbutton

--- client/src/Components/Searchbar.js
+++ client/src/Components/Searchbar.js
@@ -1,43 +1,22 @@
 import React from 'react';
 import SearchIcon from "@material-ui/icons/Search";
-import { makeStyles, fade, InputBase } from '@material-ui/core';
+import { makeStyles, fade, InputBase, IconButton } from '@material-ui/core';
 
-// Search bar example taken from: https://material-ui.com/components/app-bar/#app-bar-with-a-primary-search-field
 const useStyles = makeStyles(theme => ({
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover":{
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto"
+  search:{
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    borderRadius: "10px",
+    paddingLeft: theme.spacing(1),
+    "&:hover": {
+      backgroundColor: "rgba(255, 255, 255, 0.6)"
     }
   },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1
   },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputText: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    }
+  iconButton: {
+    padding: "0.8em"
   }
 }));
 
@@ -46,18 +25,15 @@ function Searchbar() {
 
   return (
     <div className={classes.search}>
-      <div className={classes.searchIcon}>
-        <SearchIcon />
-      </div>
       <InputBase
         placeholder="Search..."
-        classes={{
-          root: classes.inputRoot,
-          input: classes.inputText
-        }} 
+        classes={classes.input} 
         inputProps={{
-          "aria-label": "search" 
+          "aria-label": "search give back" 
         }}/>
+        <IconButton className={classes.searchIcon} aria-label="search">
+          <SearchIcon />
+        </IconButton>
     </div>
   );
 }
