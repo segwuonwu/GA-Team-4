e6af889c5f03b5226b6924d804479f8f6ecdf1a5
melissay94 <melissay94@yahoo.com>
2020-03-19 16:28:35 -0700

    Added organization page layout

--- client/src/pages/OrganizationUser.js
+++ client/src/pages/OrganizationUser.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { useState } from "react";
+import { Typography, Button, List } from "@material-ui/core";
+import EventItem from "../Components/EventComponent";
 
 function OrganizationUser() {
+
+  const [events, setEvents] = useState([]);
+
   return (
-    <div></div>
+    <div>
+      <Typography>Organization Title</Typography>
+      <Typography>Organization Bio</Typography>
+      <Button>Follow</Button>
+      <List>
+      {
+        events.forEach((item, index) => {
+          <EventItem event={item} key={index} />
+        })
+      }
+      </List>
+    </div>
   );
 }
 
