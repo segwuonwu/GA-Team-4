1040b9d7f3f19be58b2292c7604193e0fd834b6f
segwuonwu <segwuonwu@gmail.com>
2020-03-19 13:44:11 -0700

    Got most of the organization route working

--- server/routes/organizations.js
+++ server/routes/organizations.js
@@ -2,24 +2,28 @@ const express = require('express');
 const router = express.Router()
 const db = require('../models/index')
 
-// page to get a list of events
 router.get('/', (req, res) => {
-    db.Organization.find()
+    res.send('Welcome to Organization profile');
+})
+
+// page to get a list of events
+router.get('/events', (req, res) => {
+    db.Event.find()
         .then(events => {
             res.send(events);
     }).catch(err => res.send({message: 'Error in getting all events', err}))
 })
 
 //Show specific Event. 
-router.get('/:id', (req, res) => {
-    db.Organization.findById(req.params.id)
+router.get('/events/:id', (req, res) => {
+    db.Event.findById(req.params.id)
         .then(events => {
             res.send(events);
     }).catch(err => res.send({message: 'Error in getting event', err}))
 })
 
 //Take form data to add a new events
-router.post('/', (req, res) => {
+router.post('/add', (req, res) => {
     let newEvent = {
         eventname: req.body.eventname,
         eventdate: req.body.eventdate,
@@ -41,21 +45,13 @@ router.post('/', (req, res) => {
     });
 });
 
-// Edit event form
-router.get('/:id/edit', (req, res) => {
-    db.Organization.findById(req.params.id)
-    .then(event => {
-        res.render('organisations/edit', { event: event })
-    })
-})
-
 // update an event
-router.put('/:id', (req, res) => {
-    db.Organization.findById(req.params.id)
+router.put('/events/:id', (req, res) => {
+    db.Event.findById(req.params.id)
     .then(event => {
         return event.update(req.body);
     }).then(event => {
-        res.redirect(`/organizations/${event.id}`)
+        res.redirect('/organizations/events')
     }).catch(err => {
         console.log('Error Message', err);
         res.send({message :'An error occured while updating event', err});
@@ -64,15 +60,17 @@ router.put('/:id', (req, res) => {
 })
 
 // Delete an event
-router.delete('/:id', (req, res) => {
+router.delete('/events/:id', (req, res) => {
     console.log('---Delete route');
-    db.Organization.deleteOne({
-       _id: req.params.id
-    })
+    let id = { _id: req.params.id }
+    db.Event.deleteOne(id)
     .then(() => {
+        console.log('Event deleted')
         res.redirect('/organizations');
     })
     .catch(err => res.send({message: 'Error deleting event', err}))
 })
+    
+
     
 module.exports = router;
