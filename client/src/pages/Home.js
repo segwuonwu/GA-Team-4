2dd39186e341004adc4d3e88ba23ba173507cb95
melissay94 <melissay94@yahoo.com>
2020-03-20 15:22:06 -0700

    Added base for getting events, still need endpoint and base url

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Typography, Grid, Avatar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/sass/styles.scss";
 import moment from "moment";
 import SideList from "../Components/SideList";
+import ErrorMessage from "../Components/ErrorMessage";
 
 const localizer = momentLocalizer(moment);
 
@@ -15,7 +16,33 @@ const useStyles = makeStyles(theme => ({
 }));
 
 function Home(props) {
+  const [events, setEvents] = useState(null);
+  const [error, setError] = useState(null);
   const classes = useStyles();
+
+  useEffect(() => {
+    fetch()
+      .then(response => {
+        if (!response.ok) {
+          setError("Couldn't reach database");
+          return;
+        }
+        return response.json();
+      }).then(events => {
+        setEvents(events);
+      }).catch(err => {
+        setError("Couldn't retrieve events")
+        console.log(err);
+      })
+  }, []);
+
+  const eventList = () => {
+    if (events) {
+      return <SideList listType="event" events={events} />
+    } else {
+      return <ErrorMessage error={error} />
+    }
+  }
 
   return (
     <div>
@@ -31,7 +58,7 @@ function Home(props) {
       </Grid>
       <Grid container spacing={3} className={classes.root}>
         <Grid item md={3}>
-          <SideList listType="event" events={[]}/>
+          { eventList() }
         </Grid>
         <Grid item md={9}>
           <Calendar 
