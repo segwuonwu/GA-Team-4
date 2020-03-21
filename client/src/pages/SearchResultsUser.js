467032f1309351add9d4dccb6a876292efb81358
dlew253 <dlew253@yahoo.com>
2020-03-21 14:19:45 -0700

    reworking

--- client/src/pages/SearchResultsUser.js
+++ client/src/pages/SearchResultsUser.js
@@ -1,34 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Typography } from "@material-ui/core";
 import SearchResList from "../Components/SearchResList";
 import { useLocation } from "react-router-dom";
 
-
 function SearchResultsUser() {
 
-// Will get the query from the URL: https://codesandbox.io/s/react-router-query-parameters-mfh8p?from-embed
-  const useQuery = () => {
+const [results, setResults] = useState(null);  
+const [error, setError] = useState(null);
+const useQuery = () => {
     return new URLSearchParams(useLocation().search);
   }
 
-  const query = useQuery().get("query");
-  componentDidMount() {
-    fetch("https://codesandbox.io/s/react-router-query-parameters-mfh8p?from-embed")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.items
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+//"https://codesandbox.io/s/react-router-query-parameters-mfh8p?from-embed"
+  useEffect(()=>{
+      fetch(events)
+        .then(response => {
+          if (!response.ok) {
+            setError ("Sorry something went wrong");
+            return;
+          } return response.json();
+        }).then(events => {
+          setState(events);
+        }).catch(err => {
+          setError("Can't find any Events")
+          console.log(err);
+        })
+ 
+    fetch(orginizations)
+    .then(response => {
+      if (!response.ok) {
+        setError ("Oopsie Poos");
+        return;
+      } return response.json();
+    }).then(orginizations => {
+      setState(orginizations);
+    }).catch(err => {
+      setError("Cant find the Org you're looking for")
+      console.log(err);
+    })
+  },[]);
+
   }
 
   return (
@@ -36,7 +47,7 @@ function SearchResultsUser() {
       <Typography>Search Result for {query}</Typography>
       <SearchList items={[]} resultType={query} />
     </div>
-  );
+  )
 }
 
 export default SearchResultsUser;
