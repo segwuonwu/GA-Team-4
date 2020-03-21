12278c962fafade73e0c91d0b452f072b6081d1e
dlew253 <dlew253@yahoo.com>
2020-03-20 20:32:29 -0700

    search stuff, probably wrong

--- client/src/pages/SearchResultsUser.js
+++ client/src/pages/SearchResultsUser.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
-import SearchList from "../Components/SearchResList";
+import SearchResList from "../Components/SearchResList";
 import { useLocation } from "react-router-dom";
+
 
 function SearchResultsUser() {
 
@@ -11,6 +12,24 @@ function SearchResultsUser() {
   }
 
   const query = useQuery().get("query");
+  componentDidMount() {
+    fetch("https://codesandbox.io/s/react-router-query-parameters-mfh8p?from-embed")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
 
   return (
     <div>
