e4637e18b6658f6e3f7575fb4d9ed7b7f0101e70
melissay94 <melissay94@yahoo.com>
2020-03-21 11:55:38 -0700

    Added events to organizations

--- client/src/pages/OrganizationUser.js
+++ client/src/pages/OrganizationUser.js
@@ -1,24 +1,55 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Typography, Button, List } from "@material-ui/core";
 import EventItem from "../Components/EventComponent";
+import ErrorMessage from "../Components/ErrorMessage";
 
-function OrganizationUser() {
+function OrganizationUser(props) {
 
   const [events, setEvents] = useState([]);
+  const [error, setError] = useState([]);
+
+  useEffect(() => {
+    fetch("")
+      .then(response => {
+        if (!response.ok) {
+          setError("Couldn't reach database");
+          return;
+        }
+        return response.json();
+      }).then(events => {
+        if (events.length < 1) {
+          setError("No events to show right now");
+        } else {
+          setEvents(events);
+        }
+      }).catch(err => {
+        setError("Couldn't retrieve events");
+        console.log(err);
+      })
+  }, []);
+
+  const eventList = () => {
+    if (events) {
+      return (
+        <List>
+        {
+          events.map((item, index) => {
+            return <EventItem event={item} key={index} />
+          })
+        }
+        </List>
+        )
+    } else {
+      return <ErrorMessage error={error} />
+    }
+  }
 
   return (
     <div>
       <Typography>Organization Title</Typography>
       <Typography>Organization Bio</Typography>
       <Button>Follow</Button>
-      <List>
-      {
-        events.map((item, index) => {
-          return <EventItem event={item} key={index} />
-        })
-      }
-      </List>
+      {eventList()}
     </div>
   );
 }
