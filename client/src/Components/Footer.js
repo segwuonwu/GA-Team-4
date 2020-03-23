390b4eaf94d1641ef9aafd22986f8e9e80651456
melissay94 <melissay94@yahoo.com>
2020-03-23 12:19:26 -0700

    Added a sticky footer to page

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Grid, Typography, Link } from "@material-ui/core";
 
 
 //Github
@@ -6,16 +7,19 @@ import React from 'react';
 function Footer() {
     return (
       <footer className="footer">
-        <section className="section-top">
-          <div className="col">
-            <h3>SEI-29 Project 3</h3>
-            <div>Melissa Young, Solomon Egwuonwu, Shawhien Sohrabi, Dylan Lewis</div>
-            <div>https://github.com/melissay94/GA-Team-4</div>
-          </div>
-        </section>
-        <section className="section-bottom">
-          <div>Copyright</div>
-        </section>
+        <Container maxWidth="lg">
+          <Grid container>
+            <Grid item md={6}>
+              <Typography 
+                variant="subtitle2">
+                  Copyright © 2020, Melissa Young, Solomon Egwuonwu, Shawhien Sohrabi, Dylan Lewis
+              </Typography>
+            </Grid>
+            <Grid item md={4}>
+              <Link href="https://github.com/melissay94/GA-Team-4" className="footer-link">GitHub Repo</Link>
+            </Grid>
+          </Grid>
+        </Container>
       </footer>
     );
   }
