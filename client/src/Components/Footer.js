f0b52ffb50f1d0b666075979fe972da9d6e75719
melissay94 <melissay94@yahoo.com>
2020-03-23 11:48:26 -0700

    Redid some of hero to have sticky footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -5,22 +5,18 @@ import React from 'react';
 //Copyright
 function Footer() {
     return (
-      <div>
-           <footer>
-    <section className="section-top">
-      <div className="col">
-        <h3>SEI-29 Project 3</h3>
-        <div>Melissa Young, Solomon Egwuonwu, Shawhien Sohrabi, Dylan Lewis</div>
-        <div>https://github.com/melissay94/GA-Team-4</div>
-      </div>
-    </section>
-    <section className="section-bottom">
-      <div>Copyright</div>
-    </section>
-    </footer>
-
-
-      </div>
+      <footer className="footer">
+        <section className="section-top">
+          <div className="col">
+            <h3>SEI-29 Project 3</h3>
+            <div>Melissa Young, Solomon Egwuonwu, Shawhien Sohrabi, Dylan Lewis</div>
+            <div>https://github.com/melissay94/GA-Team-4</div>
+          </div>
+        </section>
+        <section className="section-bottom">
+          <div>Copyright</div>
+        </section>
+      </footer>
     );
   }
   
