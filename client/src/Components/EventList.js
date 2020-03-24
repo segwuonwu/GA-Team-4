ef56054115da3e65f5e9c6c31255356aff45527b
melissay94 <melissay94@yahoo.com>
2020-03-23 17:08:12 -0700

    Added styling to event list

--- client/src/Components/EventList.js
+++ client/src/Components/EventList.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { List, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import EventHomeItem from "./EventHomeItem";
 //host, date, time, where, # of volunteers
 
+const useStyles = makeStyles(theme => ({
+  list: {
+    marginLeft: "2em",
+    background: "#eee",
+    borderRadius: "10px",
+    minHeight: "64.5vh"
+  },
+  header: {
+    paddingTop: "1.5em",
+    marginTop: "1.5em",
+    marginLeft: "1em",
+    fontWeight: "bold",
+    color: theme.palette.primary.dark
+  }
+}));
+
 function EventList(props) {
+  const classes = useStyles();
       return(
-        <div>
-          <Typography>Upcoming Events</Typography>
+        <div
+          className={classes.list}>
+          <Typography
+            variant="h5"
+            className={classes.header}>
+              Upcoming Events
+          </Typography>
           <List>{
             props.events.map((item, index) => {
               return (<EventHomeItem event={item} key={index} />);
             })
-            }
-          </List>
+            }</List>
         </div>
       );
   }
